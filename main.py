07b20330e058a4963b15acec8e0383a120c370c9
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-03-14 15:01:38 -0600

    modif save

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import os
+from PIL import Image
 
 ruta_actual = os.path.abspath(__file__)
+archivo = 'main.py'
+ruta_actual = ruta_actual[:-len(archivo)]
 
 print(ruta_actual, type(ruta_actual))
+
+# Abre la imagen en modo RGB
+imagen = Image.open('./img/1.bmp').convert('RGB')
+
+# Convierte la imagen a YCbCr
+imagen_ycbcr = imagen.convert('YCbCr')
+
+# Muestra la imagen convertida
+imagen_ycbcr.show()
+
+# Ruta de guardado
+ruta_guardado = ruta_actual+'ImgCif'
+
+imagen_ycbcr.save("ImgCif/1Cif.jpg")

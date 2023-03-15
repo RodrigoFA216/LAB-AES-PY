d418c458769749f238b8911b47ad5b96b7d5f382
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-03-14 23:02:03 -0600

    repo actualizado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-
+# en este archivo tengo planeado que se vayan subiendo todos los cambios que ya estén para cambios finales, en dado caso que no sea así se puede hacer un archivo aparte para que se depure el code que no se entienda primero antes que nada
 ruta_actual = os.path.abspath(__file__)
 archivo = 'main.py'
 ruta_actual = ruta_actual[:-len(archivo)]

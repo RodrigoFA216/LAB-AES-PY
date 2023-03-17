1b7d1dd4ed9c3cc1550323972eae58933998043d
RodrigoFA216 <flores.rodrigo.emmanuel.dev@gmail.com>
2023-03-16 20:11:14 -0600

    Steps added

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import os
 from PIL import Image
 
+# Pasos
+# Rutas de archivos y funciones
+# >crear una variable con el nombre del archivo
+# >Establecer la ruta de este archivo
+# >A la ruta del archivo hay que quitarle en el nombre del archivo
+# >Se importan los directorios de imágenes, Cifrado AES, y LSB
+# >Seleccionar la imagen conformae a la carpeta IMG
+# >Lee la imagen y la reescala a 480x640
+# >Mostrar la imagen
+# Conversión de RGB a YCBR
+# >toma la info de color
+# >transforma esa info a YCbCr
+# >Muestra la imagen sin la información de Cb y Cr
+# >Obiene la info de alto y ancho
+# >Obtiene la info de color Cb
+# >Obtiene la info de color Cr
+# Procesamiento de la información de color
 ruta_actual = os.path.abspath(__file__)
 archivo = 'main.py'
 ruta_actual = ruta_actual[:-len(archivo)]
